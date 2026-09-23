5d0e7b86d1a2be586b96f6ccfa79655b692ec050
JackStar6677-1 <pablo.elias.miranda.292003@gmail.com>
2026-09-23 14:36:14 -0300

    feat(suites): incorporar Suite Multiverse oficial de Chagui68 (drakes-multiverse.jar) absorbiendo MultiverseCreatures y MultiverseNets

---
+++ drakes-multiverse/src/main/java/com/drakescraft/suites/multiverse/DrakesMultiversePlugin.java
@@ -0,0 +1,54 @@
+package com.drakescraft.suites.multiverse;
+
+import com.drakescraft.suites.core.DrakesCorePlugin;
+import com.drakescraft.suites.core.module.GenericSuiteModule;
+import com.drakescraft.suites.core.module.SuiteModuleManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * DrakesMultiverse - Suite Oficial del Multiverso concebida y creada por Chagui68.
+ * Absorbe y consolida de forma modular:
+ * - MultiverseCreatures (criaturas del multiverso, bosses, rituales y dimensiones)
+ * - MultiverseNets (redes digitales standalone de almacenamiento y transporte sin Slimefun)
+ * 
+ * Autor Canónico: Chagui68
+ */
+public class DrakesMultiversePlugin extends JavaPlugin {
+
+    private SuiteModuleManager moduleManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        this.moduleManager = new SuiteModuleManager(this);
+
+        // Módulos soberanos de la Suite de Chagui
+        this.moduleManager.registerModule(new GenericSuiteModule(this, "creatures", "Multiverse Creatures & Bosses (por Chagui68)"));
+        this.moduleManager.registerModule(new GenericSuiteModule(this, "nets", "Multiverse Standalone Digital Networks (por Chagui68)"));
+
+        this.moduleManager.enableAll();
+
+        DrakesCorePlugin core = DrakesCorePlugin.getInstance();
+        if (core != null) {
+            core.getSuiteRegistry().registerSuite("multiverse", this, this.moduleManager);
+        }
+
+        getLogger().info("§5[DrakesMultiverse] §dSuite Multiverse de Chagui68 v" + getPluginMeta().getVersion() + " inicializada con exito.");
+    }
+
+    @Override
+    public void onDisable() {
+        if (moduleManager != null) {
+            moduleManager.disableAll();
+        }
+        DrakesCorePlugin core = DrakesCorePlugin.getInstance();
+        if (core != null) {
+            core.getSuiteRegistry().unregisterSuite("multiverse");
+        }
+        getLogger().info("[DrakesMultiverse] Suite de Chagui68 deshabilitada limpiamente.");
+    }
+
+    public SuiteModuleManager getModuleManager() {
+        return moduleManager;
+    }
+}
